ec2b50767402fcb47b0dbed62c853e8d55feff3d
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-30 01:11:05 +0900

    Fix: 메인 영상 날짜 n일전 표기로 변경

--- src/components/common/VideoCard.jsx
+++ src/components/common/VideoCard.jsx
@@ -12,11 +12,41 @@ const VideoCard = ({
     brandingSettings,
   },
 }) => {
+  const elapsedTime = (date) => {
+    const start = new Date(date);
+    const end = new Date();
+
+    const diff = (end - start) / 1000;
+
+    const times = [
+      { name: "년", milliSeconds: 60 * 60 * 24 * 365 },
+      { name: "개월", milliSeconds: 60 * 60 * 24 * 30 },
+      { name: "일", milliSeconds: 60 * 60 * 24 },
+      { name: "시간", milliSeconds: 60 * 60 },
+      { name: "분", milliSeconds: 60 },
+    ];
+
+    for (const value of times) {
+      const betweenTime = Math.floor(diff / value.milliSeconds);
+
+      if (betweenTime > 0) {
+        return `${betweenTime}${value.name} 전`;
+      }
+    }
+    return "방금 전";
+  };
+
+  const test = (date) => {
+    console.log("date : ", date.slice(0, 10));
+  };
+
+  // elapsedTime('2022-11-15');
   return (
     <Link to={`/video/${videoId}`} className="box">
       <img src={snippet?.thumbnails?.medium?.url} alt={snippet?.title} />
       <div className="boxInfo">
-        <p className="date">{snippet?.publishedAt}</p>
+        <p className="date">{elapsedTime(snippet?.publishedAt.slice(0, 10))}</p>
+        <p className="date">{snippet?.publishedAt.slice(0, 10)}</p>
         <p className="title">{snippet?.title}</p>
         <p className="desc">{snippet?.description}</p>
         <Link to={`/channel/${snippet.channelId}`}>

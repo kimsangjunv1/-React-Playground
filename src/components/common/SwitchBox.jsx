9bc54221c6a3d6e6370ac49217b6dc581c4b4f12
kimsangjunv1 <ingyoe64@gmail.com>
2023-08-29 16:46:25 +0900

    Fix: title 컴포넌트 props 여부에 따라 표시되는 방식 수정

---
+++ src/components/common/SwitchBox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SwitchBox = () => {
+  return (
+    <div className="Switch_container">
+      <button className="item">TRUE</button>
+      <button className="item">FALSE</button>
+    </div>
+  );
+};
+
+export default SwitchBox;
--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import icon_recommand from "./../../assets/img/icon_recommand.svg";
 
 const Title = ({ categorys }) => {
   return (
-    <h2>
-      <em>{categorys}</em>asdasds
-    </h2>
+    <div className="title_container">
+      {!categorys ? (
+        <div className="title_container_inner">
+          <img src={icon_recommand} alt="추천 아이콘" />
+          <h2 className="title">RECOMMANDED CHANNEL</h2>
+          <p className="sub_title">추천하는 채널이에요!</p>
+        </div>
+      ) : (
+        <div className="title_container_inner">
+          <img src={icon_recommand} alt="추천 아이콘" />
+          <h2 className="title">{categorys}의 결과</h2>
+          <p className="sub_title">{categorys}채널 검색 결과에요!</p>
+        </div>
+      )}
+    </div>
   );
 };
 

c27ce98aa49a85098c4093a967a63458b77f0a14
kimsangjunv1 <ingyoe64@gmail.com>
2023-11-20 05:20:11 +0900

    Fix: 메인 타이틀 수정

--- src/components/common/Title.jsx
+++ src/components/common/Title.jsx
@@ -14,11 +14,20 @@ const Title = ({ categorys }) => {
 					{/* <p className="sub_title">추천하는 영상이에요!</p> */}
 				</div>
 			) : (
-				<div className="title_container_inner">
-					<img src={icon_recommand} alt="추천 아이콘" />
-					<h2 className="title">{categorys}</h2>
-					<p className="sub_title">{categorys}와 관련된 검색 결과입니다.</p>
-				</div>
+				<>
+					{categorys !== "태블릿 갤럭시 아이폰" ? (
+						<div className="title_container_inner">
+							<img src={icon_recommand} alt="추천 아이콘" />
+							<h2 className="title">{categorys}</h2>
+							<p className="sub_title">{categorys}와 관련된 검색 결과입니다.</p>
+						</div>
+					) : (
+						<div className="title_container_inner">
+							<img src={icon_recommand} alt="추천 아이콘" />
+							<h2 className="title">최신영상</h2>
+						</div>
+					)}
+				</>
 			)}
 		</div>
 	);
